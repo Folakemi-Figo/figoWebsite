f4cfdc9f78dd5ad6b0b1fc75ce0cd72f8d1bfac2
Folakemi-Figo <folake@figo.africa>
2022-08-01 22:45:04 +0100

    Code Input Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FigoAgentComponent } from './figo-agent/figo-agent.component';
+import { AgentLoginComponent } from './login/agent-login/agent-login.component';
+import { LoginComponent } from './login/login.component';
+import { MerchantLoginComponent } from './login/merchant-login/merchant-login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WebsiteComponent } from './website/website.component';
 
 const routes: Routes = [
-  {path: 'home', component: FigoAgentComponent},
+  {path: 'home', component: WebsiteComponent},
 { path: '',  redirectTo: 'home', pathMatch: 'full'},
+
+
+{
+  path: 'login',
+  loadChildren: () => import('./login/login-routing.module').then(m => m.LoggingRoutngModule)
+},
+
+
+{ path: 'page-not-found', component: PageNotFoundComponent },
+{ path: '**', component: PageNotFoundComponent },
+
 ];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FigoAgentComponent } from './figo-agent/figo-agent.component';
+import { WebsiteComponent } from './website/website.component';
+import { LoginComponent } from './login/login.component';
+import { AgentLoginComponent } from './login/agent-login/agent-login.component';
+import { MerchantLoginComponent } from './login/merchant-login/merchant-login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FigoAgentComponent
+    WebsiteComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -19,6 +25,7 @@ import { FigoAgentComponent } from './figo-agent/figo-agent.component';
     HttpClientModule,
     RouterModule,
     NgbModule,
+    LoginModule,
     AppRoutingModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
